32ec658c03763ab2143e0ed34c2cde9a7b13a0fe
Dan Le <tle57@gmu.edu>
2022-05-07 03:22:11 -0400

    add input for file name and check if existing

--- FinAidCalc.py
+++ FinAidCalc.py
@@ -1,5 +1,17 @@
-def TextParser():
+import os
+
+
+def Main():
+    TextParser(input("Enter filename: "))
+
+
+def TextParser(filename):
+    if not os.path.isfile(filename):
+        print('File does not exist! Quitting program.')
+        quit()
+    pfile = open(filename, "r")
     print('test')
+    pfile.close()
 
 
-TextParser()
+Main()
